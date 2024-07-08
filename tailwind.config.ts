293702c36172b7eefe4a567e6ded372d9f0fdc01
Joe Ravikumar <55877245+Joe-Ravikumar@users.noreply.github.com>
2024-07-08 07:53:08 +0530

    Centered

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from "tailwindcss";
+import { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#87ceeb",
+        secondary: "#ffffff",
+        accent: "#ff6f61",
+        foreground: "rgb(var(--foreground-rgb))",
+        backgroundStart: "rgb(var(--background-start-rgb))",
+        backgroundEnd: "rgb(var(--background-end-rgb))",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
