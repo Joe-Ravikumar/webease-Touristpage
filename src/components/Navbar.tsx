0ae3b764f408e14c2ac97f6e49acc5e7ac40be5f
Nusnan .A.M <82087528+mohamednusnan@users.noreply.github.com>
2024-07-19 07:44:30 +0530

    nes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,15 +25,15 @@ const Navbar: React.FC = () => {
             role="navigation"
           >
             {/*      <!-- Brand logo --> */}
-            <div className="flex items-center">
+            <div className="flex items-center font-semibold">
               <a
                 id="WindUI"
                 aria-label="WindUI logo"
                 aria-current="page"
-                className="flex items-center gap-2 whitespace-nowrap py-3 text-lg focus:outline-none lg:flex-1"
+                className="flex items-center gap-2 whitespace-nowrap py-3 text-lg font-bold  focus:outline-none lg:flex-1"
                 href="/"
               >
-                WebEase
+              Place Tricker
               </a>
             </div>
             {/*      <!-- Mobile trigger --> */}
@@ -89,7 +89,7 @@ const Navbar: React.FC = () => {
                   role="menuitem"
                   aria-haspopup="false"
                   className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-[#8781DB] focus:text-[#80BBFF] focus:outline-none focus-visible:outline-none lg:px-8"
-                  href="/portfolio"
+                  href="/destination"
                 >
                   <span>Destination</span>
                 </a>
@@ -154,27 +154,13 @@ const Navbar: React.FC = () => {
                   <span className="sr-only"> 7 new emails </span>
                 </span>
               </a> */}
-              <div className="pr-8 flex items-center">
-                <button>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="2em"
-                    height="12em"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fill="#80BBFF"
-                      d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
+
               <div className="lex items-center hidden xl:flex">
                 <Button
-                  btnText="Get Started"
+                  btnText="Get Connect"
                   border="none"
                   borderColor="white"
-                  backgroundColor="#80BBFF"
+                  backgroundColor="#6b6e6c"
                   fontColor="white"
                   click={handleLogin}
                 />
