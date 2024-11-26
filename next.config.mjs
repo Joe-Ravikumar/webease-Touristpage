4a6646b699d20f223fc4ee4f66248c51a77619f4
pradisharoon <k.pradeeshan4@gmail.com>
2024-11-26 13:07:38 +0530

    image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["files.edgestore.dev"], // Add your external image domain here
+  },
+};
 
 export default nextConfig;
